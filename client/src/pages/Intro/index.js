458cbfa08e535167423525af73d1526ee771911a
YUZAMIN <untmd.crmn@gmail.com>
2021-05-19 01:27:23 +0900

    Feat : App.css font 4종 추가

--- client/src/pages/Intro/index.js
+++ client/src/pages/Intro/index.js
@@ -7,7 +7,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
-    backgroundImage: `url(${'/images/intro/white_brick6.jpg'})`,
+    backgroundImage: `url(${'/images/intro/white_brick6_3.png'})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
   },
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
 function Intro() {
   const classes = useStyles();
 
-  return <div className={classes.root}>ㅎㅇㅎㅇㅎㅇ</div>;
+  return <div className={classes.root}>베이커가의 옷장</div>;
 }
 
 export default Intro;

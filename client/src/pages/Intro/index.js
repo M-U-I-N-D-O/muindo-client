47743f015a406f6f26943653e36fba552c7dd322
YUZAMIN <untmd.crmn@gmail.com>
2021-05-19 16:51:51 +0900

    Feat : Intro Page 1차 작업 완료 추후 (문 또는 간판 클릭 시) Main Page로 이동하도록 수정 예정

--- client/src/pages/Intro/index.js
+++ client/src/pages/Intro/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-// import whiteBrick from '../../../public/images/intro/white_brick.jpg';
-
-// import whiteBrick from '';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -11,15 +9,62 @@ const useStyles = makeStyles((theme) => ({
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
   },
-  container: {
-    height: '100vh',
+  introElements: {
+    display: 'flex',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // height: '200px',
+  },
+  signBox: {
+    display: 'flex',
+    marginTop: '70px',
+    width: '400px',
+    height: '100px',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#323B48',
+    position: 'fixed',
+    bottom: '580px',
+  },
+  signText: {
+    fontFamily: 'Lobster',
+    fontSize: '55px',
+    color: '#E2B063',
+  },
+  door: {
+    width: '250px',
+    height: '500px',
+    position: 'fixed',
+    bottom: '40px',
+  },
+  doorBottom: {
+    width: '360px',
+    height: '40px',
+    backgroundColor: '#29303A',
+    position: 'fixed',
+    bottom: '0',
   },
 }));
 
 function Intro() {
   const classes = useStyles();
 
-  return <div className={classes.root}>셜록 옷즈 Sherlock Odds</div>;
+  return (
+    <div className={classes.root}>
+      <Link className={classes.introElements} to="/">
+        <div className={classes.signBox}>
+          <h1 className={classes.signText}>Sherlock Odd</h1>
+          <img
+            className={classes.door}
+            alt=""
+            src="/images/intro/intro_door.jpg"
+          />
+          <div className={classes.doorBottom}></div>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
 export default Intro;

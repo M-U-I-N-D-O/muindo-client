e7b7ccd952cb7ca7cef1f96cb10957103936ec2d
WI Young Min <youngminieo1005@gmail.com>
2021-05-20 13:34:51 +0900

    Feat : Navbar,Footer,Sidebar 1차 구현

---
+++ client/src/pages/About/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Abuot() {
+  return (
+    <div>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+      <h1>This is About Page</h1>
+    </div>
+  );
+}
+
+export default Abuot;
---
+++ client/src/pages/AnalysisColor/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function AnalysisColor() {
+  return (
+    <div>
+      <h1>This is 왓슨의 믹스매치 Page</h1>
+      <h1>This is 왓슨의 믹스매치 Page</h1>
+      <h1>This is 왓슨의 믹스매치 Page</h1>
+      <h1>This is 왓슨의 믹스매치 Page</h1>
+      <h1>This is 왓슨의 믹스매치 Page</h1>
+    </div>
+  );
+}
+
+export default AnalysisColor;

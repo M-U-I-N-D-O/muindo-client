533be8fb9967cbb53539ecf19a84448c935ef1d0
YUZAMIN <untmd.crmn@gmail.com>
2021-05-20 20:36:34 +0900

    Feat : Main Page layout 구축

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,13 +1,63 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+    backgroundImage: `url(${'/images/main/sherlock_wall.png'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+  container: {
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center',
+
+    // backgroundColor: 'red',
+  },
+  contentsBox: {
+    display: 'flex',
+    backgroundColor: 'white',
+    width: '768px',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+  },
+  menuButton: {
+    width: '220px',
+    height: '220px',
+    backgroundColor: 'blue',
+    margin: '60px 30px',
+  },
+}));
+
+function MenuButton() {
+  const classes = useStyles();
+  return (
+    <div className={classes.menuButton}>
+      <h1>버튼</h1>
+    </div>
+  );
+}
 
 function Home() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>This is HomePage</h1>
-      <h1>This is HomePage</h1>
-      <h1>This is HomePage</h1>
-      <h1>This is HomePage</h1>
-      <h1>This is HomePage</h1>
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <div className={classes.contentsBox}>
+          <div>
+            <MenuButton />
+            <MenuButton />
+          </div>
+          <div>
+            <MenuButton />
+            <MenuButton />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

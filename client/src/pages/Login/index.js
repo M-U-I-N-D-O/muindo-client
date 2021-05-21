27575ff73b69934deef1b5e3c42798076c835945
YUZAMIN <untmd.crmn@gmail.com>
2021-05-21 15:52:17 +0900

    Feat : Login Page layout 구축

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+    backgroundImage: `url(${'/images/home/sherlock_wall.png'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+  container: {
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center',
+  },
+  contentsBox: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: '3.5rem',
+    paddingTop: '5rem',
+    backgroundColor: 'white',
+    width: '80vw',
+    maxWidth: '768px',
+    overflow: 'hidden',
+  },
+}));
 
 function Login() {
+  const classes = useStyles();
+
   return (
-    <div style={{ paddingBottom: '3.5rem', paddingTop: '5rem' }}>
-      <h1>Login Page</h1>
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <div className={classes.contentsBox}></div>
+      </div>
     </div>
   );
 }

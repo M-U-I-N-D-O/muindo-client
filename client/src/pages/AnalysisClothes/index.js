a31ba89408669075509a89e1d7416d4863994405
WI Young Min <youngminieo1005@gmail.com>
2021-05-23 03:58:08 +0900

    Feat : 셜록의 돋보기 - 이미지첨부모드(Upload,Drag & Drop / Copy & Paste) , Copy & Paste 보류

--- client/src/pages/AnalysisClothes/index.js
+++ client/src/pages/AnalysisClothes/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
+import { Gluejar } from '@charliewilco/gluejar';
 import axios from 'axios';
 import { useHistory } from 'react-router';
 import styled from 'styled-components';
@@ -10,16 +11,23 @@ function AnalysisClothes() {
   // fileUrl = 파일객체
   // imgFile = file이라는 이름의 arument에 file객체를 담고, API서버(flask)로 POST요청
   // 두 정보모두 state, redux에 저장할 필요가 있다면, 변경할 것
+  // modeButton = 첨부방식(업로드,드래그앤드랍모드 / 복붙모드)
   const [fileUrl, setFileUrl] = useState(null);
   const [imgFile, setImgFile] = useState(null);
+  const [modeButton, setModeButton] = useState(true);
 
+  // Copy & Paste 첨부방식 << 에러가 많은데, 빠질 경우, 삭제
+  const [copyFileUrl, setCopyFileUrl] = useState(null);
+  const [copyImgFile, setCopyImgFile] = useState(null);
+
+  // Drag & Drop 방식, Process
   const onDrop = useCallback(async (acceptedFiles) => {
     // 이미지 미리보기 처리(썸네일)
     const imageFile = acceptedFiles[0];
     setImgFile(imageFile);
     const imageUrl = URL.createObjectURL(imageFile);
-    console.log('imageFile :', imageFile);
-    console.log('fileUrl :', imageUrl);
+    console.log('드래그 앤 드랍 imageFile :', imageFile);
+    console.log('드래그 앤 드랍 fileUrl :', imageUrl);
     setFileUrl(imageUrl);
 
     // ★ 아래에, handleFilePost() 실행하도록 할 것
@@ -51,20 +59,22 @@ function AnalysisClothes() {
     ...getRootProps(),
   };
 
+  // 첨부하기 방식 Process
   function processImage(event) {
     if (event.target.files.length !== 0) {
       const imageFile = event.target.files[0];
       setImgFile(imageFile);
       const imageUrl = URL.createObjectURL(imageFile);
-      //   console.log('imageFile :', imageFile);
-      //   console.log(imageUrl);
+      console.log('첨부하기 imageFile :', imageFile);
+      console.log('첨부하기 imageUrl :', imageUrl);
       setFileUrl(imageUrl);
     }
   }
 
+  // 첨부된 이미지, 제출시 호출
   function handleFilePost() {
     const formData = new FormData();
-    console.log('imgFile :', imgFile);
+    console.log('제출 imgFile :', imgFile);
     formData.append('file', imgFile);
 
     // return axios.post("/api/upload", formData).then(res => {
@@ -73,48 +83,116 @@ function AnalysisClothes() {
     //   alert('실패')
     // })
   }
+
+  // Copy & Paste Process
+  const process = async (selectedImage) => {
+    try {
+      async function createFile(Myfile) {
+        let response = await fetch(Myfile);
+        let data = await response.blob();
+        let metadata = {
+          type: 'image/png',
+        };
+        let file = new File([data], 'test.png', metadata);
+        // console.log('복사 붙여넣기 selected image : ', selectedImage);
+        const imageFile = file;
+        setCopyImgFile(imageFile);
+        const imageUrl = URL.createObjectURL(imageFile);
+        console.log('복사 붙여넣기 file :', imageFile);
+        console.log('복사 붙여넣기 imageUrl :', imageUrl);
+        setCopyFileUrl(imageUrl);
+      }
+      createFile(selectedImage);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Container>
       <TopComment />
-
       <ImageContainer>
-        <ImageBox>
-          {fileUrl ? (
-            <img src={fileUrl} alt={`${fileUrl}`} />
-          ) : (
-            <div
-              {...RootProps}
-              multiple={false}
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              <input {...InputProps} />
-              <div>이미지를 드래그해서 놓아보세요.</div>
-            </div>
-          )}
-        </ImageBox>
+        {modeButton && (
+          <ImageBox>
+            {fileUrl ? (
+              <img src={fileUrl} alt={`${fileUrl}`} />
+            ) : (
+              <div
+                {...RootProps}
+                multiple={false}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <input {...InputProps} />
+                <div>이미지를 드래그해서 놓아보세요.</div>
+              </div>
+            )}
+          </ImageBox>
+        )}
+
+        {!modeButton && (
+          <ImageBox>
+            {copyFileUrl ? (
+              <Gluejar
+                onPaste={(files) => {
+                  process(files);
+                }}
+                onError={(err) => console.error(err)}
+              >
+                {({ images }) =>
+                  images.length > 0 &&
+                  images.map((image) => (
+                    <img src={image} key={image} alt={`Pasted: ${image}`} />
+                  ))
+                }
+              </Gluejar>
+            ) : (
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                여기에 Copy AND Paste를 해주세요.
+              </div>
+            )}
+          </ImageBox>
+        )}
       </ImageContainer>
 
       <ButtonContainer>
         {fileUrl === null && (
           <div style={{ padding: '2vh' }}>
-            <LuxuryLabelBtn htmlFor="input-file">
-              이미지 업로드하기
-            </LuxuryLabelBtn>
-            <input
-              type="file"
-              id="input-file"
-              accept="image/*"
-              onChange={processImage}
-              style={{ display: 'none' }}
-            />
+            {modeButton && (
+              <div>
+                <LuxuryLabelBtn htmlFor="input-file">
+                  이미지 업로드하기
+                </LuxuryLabelBtn>
+                <input
+                  type="file"
+                  id="input-file"
+                  accept="image/*"
+                  onChange={processImage}
+                  style={{ display: 'none' }}
+                />
+              </div>
+            )}
+            <div>
+              <LuxuryBtn
+                onClick={() => {
+                  setModeButton(!modeButton);
+                }}
+              >
+                첨부방식 변경하기
+              </LuxuryBtn>
+            </div>
           </div>
         )}
 
         {fileUrl !== null && (
-          <>
+          <div style={{ padding: '2vh' }}>
             <div>
               <LuxuryBtn
                 onClick={() => {
@@ -137,7 +215,7 @@ function AnalysisClothes() {
                 이미지 분석하기
               </LuxuryBtn>
             </div>
-          </>
+          </div>
         )}
       </ButtonContainer>
     </Container>

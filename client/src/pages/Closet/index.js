d41025d04a2188535536f95969e446080b84b6c9
YUZAMIN <untmd.crmn@gmail.com>
2021-05-24 03:01:15 +0900

    Feat : 베이커가 옷장 안내 문구 및 이미지 삽입

--- client/src/pages/Closet/index.js
+++ client/src/pages/Closet/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import TopComment from '../../components/AnalysisClothes/topComment';
+
 const useStyles = makeStyles((theme) => ({
-  root: {},
+  root: {
+    display: 'flex',
+    // justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  title: {
+    marginTop: '20px',
+  },
+  closetImg: {
+    width: '700px',
+  },
 }));
 
 function Closet() {
   const classes = useStyles();
 
   return (
-    <>
-      <div>
+    <div className={classes.root}>
+      <div className={classes.title}>
+        <TopComment comment={'옷장을 클릭해 열어보세요.'} />
+      </div>
+      <img className={classes.closetImg} alt="" src="/images/closet/closet_open.jpg" />
+
+      {/* <div>
         <h1>This is Closet Page</h1>
         <h1>This is Closet Page</h1>
         <h1>This is Closet Page</h1>
@@ -21,8 +38,8 @@ function Closet() {
         <h1>This is Closet Page</h1>
         <h1>This is Closet Page</h1>
         <h1>This is Closet Page</h1>
-      </div>
-    </>
+      </div> */}
+    </div>
   );
 }
 

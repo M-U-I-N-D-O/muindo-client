428fd5c28cdfc405145af93accefc4d142f56dc5
YUZAMIN <untmd.crmn@gmail.com>
2021-06-02 03:47:22 +0900

    Feat : closetModal filter 기능 구축(디자인 미적용)

--- client/src/components/Closet/groupSelector1.js
+++ client/src/components/Closet/groupSelector1.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { ModalContext } from '../../App';
 import axios from 'axios';
@@ -17,67 +17,58 @@ export default function GroupSelector1() {
   const classes = useStyles();
   const { condition, setCondition } = useContext(ModalContext);
   const { modalMode, setModalMode } = useContext(ModalContext);
-  const [selectorList1, setSelectorList1] = useState([]);
-  const [selectorList2, setSelectorList2] = useState([]);
-  const [selectorList3, setSelectorList3] = useState([]);
-  const [selectorList4, setSelectorList4] = useState([]);
+  const [categoryList, setCategoryList] = useState([]);
+  const [subCategoryList, setSubCategoryList] = useState([]);
+  const [brandList, setBrandList] = useState([]);
   const [conditionNum, setConditionNum] = useState(100);
   const fetch = useEffect(() => {
     try {
       axios.get('http://localhost:3000/data/closetCondition.json').then((res) => {
         let result = res.data.filter_query;
         result = result[modalMode].middle_category;
-        setSelectorList1(result);
-        setSelectorList2(result);
-        setSelectorList3(result);
-        setSelectorList4(result);
+
+        let categoryArr = [];
+        for (var i = 0; i < result.length; i++) {
+          categoryArr.push(result[i]['category']);
+        }
+
+        let subCategoryArr = [];
+        for (var j = 0; j < result.length; j++) {
+          subCategoryArr.push(result[j]['sub_category']);
+        }
+        let brandArr = [];
+        for (var l = 0; l < result.length; l++) {
+          brandArr.push(result[l]['brand']);
+        }
+
+        setCategoryList(categoryArr);
+        setSubCategoryList(subCategoryArr);
+        setBrandList(brandArr);
       });
     } catch (err) {
       console.log(err);
     }
-  }, []);
-  // console.log(selectorList);
-  console.log(selectorList1);
-  let a = [];
-  for (var i = 0; i < selectorList1.length; i++) {
-    a.push(selectorList1[i]['category']);
-  }
+  }, [condition]);
 
-  let b = [];
-  for (var j = 0; j < selectorList2.length; j++) {
-    b.push(selectorList2[j]['sub_category']);
-  }
-  console.log(b);
+  const handleInitialize = (event) => {
+    var newCondition = { ...condition };
+    delete newCondition['category'];
+    delete newCondition['sub_category'];
+    delete newCondition['brand'];
+    setCondition(newCondition);
+  };
 
-  // let c = [];
-  // for (var k = 0; k < selectorList3.length; k++) {
-  //   c.push(selectorList3[k]['price']);
-  // }
-  // console.log(c);
-
-  let d = [];
-  for (var l = 0; l < selectorList4.length; l++) {
-    d.push(selectorList4[l]['brand']);
-  }
-  console.log(d);
-
-  // for (var i = 0; i < selectorList.length; i++) {
-  //   a.push(selectorList[i][kind]);
-  // }
-
-  console.log(a);
-
-  const handleChange1 = (event) => {
+  const handleChangeCategory = (event) => {
     if (event.target.value) {
       var newCondition = { ...condition };
       newCondition['category'] = event.target.value;
-      setConditionNum(a.indexOf(event.target.value));
+      setConditionNum(categoryList.indexOf(event.target.value));
       console.log(condition);
       setCondition(newCondition);
     }
   };
 
-  const handleChange2 = (event) => {
+  const handleChangeSubCategory = (event) => {
     if (event.target.value) {
       var newCondition = { ...condition };
       newCondition['sub_category'] = event.target.value;
@@ -85,15 +76,7 @@ export default function GroupSelector1() {
       setCondition(newCondition);
     }
   };
-  // const handleChange3 = (event) => {
-  //   if (event.target.value) {
-  //     var newCondition = { ...condition };
-  //     newCondition['price'] = event.target.value;
-  //     console.log(condition);
-  //     setCondition(newCondition);
-  //   }
-  // };
-  const handleChange4 = (event) => {
+  const handleChangeBrand = (event) => {
     if (event.target.value) {
       var newCondition = { ...condition };
       newCondition['brand'] = event.target.value;
@@ -106,25 +89,21 @@ export default function GroupSelector1() {
     <div>
       <FormControl>
         <InputLabel>category</InputLabel>
-        <Select value="category" onChange={handleChange1}>
-          {a.map(function (selector, i) {
-            return <MenuItem value={selector}>{selector}</MenuItem>;
-          })}
-          {/* {selectorList.map(function (selector, i) {
-            selector[kind].map(function (obj, j) {
-              // console.log(obj);
-              return <MenuItem value="obj">{obj}</MenuItem>;
-            });
-          })} */}
-          {/* <MenuItem value={'디스이즈네버댓'}>디스이즈네버댓</MenuItem>
-          <MenuItem value={''}>미정</MenuItem> */}
+        <Select value="category" onChange={handleChangeCategory}>
+          {condition['sub_category'] || condition['brand'] ? (
+            <MenuItem onClick={handleInitialize}>분류 초기화</MenuItem>
+          ) : (
+            categoryList.map(function (selector, i) {
+              return <MenuItem value={selector}>{selector}</MenuItem>;
+            })
+          )}
         </Select>
       </FormControl>
       <FormControl>
         <InputLabel>sub-category</InputLabel>
-        <Select value="sub_category" onChange={handleChange2}>
+        <Select value="sub_category" onChange={handleChangeSubCategory}>
           {conditionNum !== 100 ? (
-            b[conditionNum].map(function (selector, i) {
+            subCategoryList[conditionNum].map(function (selector, i) {
               return <MenuItem value={selector}>{selector}</MenuItem>;
             })
           ) : (
@@ -132,24 +111,12 @@ export default function GroupSelector1() {
           )}
         </Select>
       </FormControl>
-      {/* <FormControl>
-        <InputLabel>price</InputLabel>
-        <Select value="price" onChange={handleChange3}>
-          {conditionNum !== 100 ? (
-            c[conditionNum].map(function (selector, i) {
-              return <MenuItem value={selector}>{selector}</MenuItem>;
-            })
-          ) : (
-            <div></div>
-          )}
-        </Select>
-      </FormControl> */}
 
       <FormControl>
         <InputLabel>brand</InputLabel>
-        <Select value="brand" onChange={handleChange4}>
+        <Select value="brand" onChange={handleChangeBrand}>
           {conditionNum !== 100 ? (
-            d[conditionNum].map(function (selector, i) {
+            brandList[conditionNum].map(function (selector, i) {
               return <MenuItem value={selector}>{selector}</MenuItem>;
             })
           ) : (

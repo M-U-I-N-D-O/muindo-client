2b5a39f9b1c6992449e910ba82168a8a1865634a
YUZAMIN <untmd.crmn@gmail.com>
2021-06-11 21:14:55 +0900

    Feat : AboutPage component화, 노션 포트폴리오 주소 추가

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -1,8 +1,29 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
+
+function Members(props) {
+  return (
+    <ImgBoxItem>
+      <img src={props.profileImg} alt="member" width="120px" height="120px" />
+      <ImgNameText>{props.name}</ImgNameText>
+      <ImgRoleText>{props.role}</ImgRoleText>
+      <ImgLink href={props.gitHubLink} target="_blank">
+        <IconButton>
+          <GitHubIcon />
+        </IconButton>
+      </ImgLink>
+      <ImgLink href={props.notionLink} target="_blank">
+        <IconButton>
+          <AccountCircleRoundedIcon style={{ fontSize: '29px' }} />
+        </IconButton>
+      </ImgLink>
+    </ImgBoxItem>
+  );
+}
 
 function About() {
   return (
@@ -27,48 +48,36 @@ function About() {
 
       <ImgBoxText>Contributor</ImgBoxText>
       <ImgBox>
-        <ImgBoxItem>
-          <img src="/images/about/1.png" alt="minoj" width="120px" height="120px" />
-          <ImgNameText>정성헌</ImgNameText>
-          <ImgRoleText>BE / AI</ImgRoleText>
-          <ImgLink href="https://github.com/Heon4856" target="_blank">
-            <IconButton>
-              <GitHubIcon />
-            </IconButton>
-          </ImgLink>
-        </ImgBoxItem>
-        <ImgBoxItem>
-          <img src="/images/about/2.png" alt="minoj" width="120px" height="120px" />
-          <ImgNameText>김수람</ImgNameText>
-          <ImgRoleText>BE</ImgRoleText>
-          <ImgLink href="https://github.com/su-ram" target="_blank">
-            <IconButton>
-              <GitHubIcon />
-            </IconButton>
-          </ImgLink>
-        </ImgBoxItem>
+        <Members
+          name="정성헌"
+          role="BE / AI"
+          profileImg="/images/about/1.png"
+          gitHubLink="https://github.com/Heon4856"
+          notionLink="https://www.notion.so/gosi/65853100963a459faa486fa881a4112c"
+        />
+        <Members
+          name="김수람"
+          role="BE"
+          profileImg="/images/about/2.png"
+          gitHubLink="https://github.com/su-ram"
+          notionLink="https://www.notion.so/gosi/65853100963a459faa486fa881a4112c"
+        />
       </ImgBox>
       <ImgBox>
-        <ImgBoxItem>
-          <img src="/images/about/3.png" alt="minoj" width="120px" height="120px" />
-          <ImgNameText>민유지</ImgNameText>
-          <ImgRoleText>FE</ImgRoleText>
-          <ImgLink href="https://github.com/nvrtmd" target="_blank">
-            <IconButton>
-              <GitHubIcon />
-            </IconButton>
-          </ImgLink>
-        </ImgBoxItem>
-        <ImgBoxItem>
-          <img src="/images/about/4.png" alt="minoj" width="120px" height="120px" />
-          <ImgNameText>위영민</ImgNameText>
-          <ImgRoleText>FE</ImgRoleText>
-          <ImgLink href="https://github.com/youngminss" target="_blank">
-            <IconButton>
-              <GitHubIcon />
-            </IconButton>
-          </ImgLink>
-        </ImgBoxItem>
+        <Members
+          name="민유지"
+          role="FE"
+          profileImg="/images/about/3.png"
+          gitHubLink="https://github.com/nvrtmd"
+          notionLink="https://www.notion.so/FrontEnd-Engineer-166a3ec16a834a9c9c0532ece9bc2f8d"
+        />
+        <Members
+          name="위영민"
+          role="FE"
+          profileImg="/images/about/4.png"
+          gitHubLink="https://github.com/youngminss"
+          notionLink="https://www.notion.so/a5318dc83cf7495cab136272b451ed6b"
+        />
       </ImgBox>
     </Container>
   );
